e856db702f8860783424fc44f13b209c173a3bde
Yue Gao <hologerry@gmail.com>
2021-09-11 21:39:33 +0800

    add configs

---
+++ scripts/dist_train_rx50_pseg_gen_mobilenet_small_bs2.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+set -x
+
+exper_name='pseg_scope40_rx50_gen_mobilenet_small_bs2'
+
+master_addr=${MASTER_IP}
+master_port=28652
+
+python -m torch.distributed.launch --nproc_per_node=8 \
+    --nnodes ${OMPI_COMM_WORLD_SIZE} --node_rank ${OMPI_COMM_WORLD_RANK} --master_addr ${master_addr} --master_port ${master_port} \
+    RTNet/train.py \
+    --data_root ./data \
+    --exper_name ${exper_name} \
+    --batch_size 2 \
+    --dataset_name_mode 0 \
+    --sub_dataset_mode 1 \
+    --backbone RX50 \
